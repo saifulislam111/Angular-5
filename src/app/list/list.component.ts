684a740d447463933e2f112939a49a92ac937644
saifulislam111 <saiful.islam@televersant.com>
2018-07-10 12:39:04 +0600

    flez layout

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -9,6 +9,8 @@ export class ListComponent implements OnInit {
 
   selectedHero:any;
 
+  title = 'Flex Layout App'
+
 
   obj = {
     firstname: "saiful",
@@ -23,6 +25,17 @@ export class ListComponent implements OnInit {
     {name: "Gugly", id: 3},
     {name: "NJ", id: 4}
   ]
+
+  pictures = [
+  {id:1, caption: '1st image', img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/U9PP3KXXY2.jpg'},
+  {id:2, caption: '2nd image', img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/R926LU1YEA.jpg'},
+  {id:3, caption: '3rd image', img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/LTLE4QGRVQ.jpg'},
+  {id:4, caption: '4th image', img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/8V46UZCS0V.jpg'},
+  {id:5, caption: '5th image', img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/U9PP3KXXY2.jpg'},
+  {id:6, caption: '6th image', img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/R926LU1YEA.jpg'},
+  {id:7, caption: '7th image', img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/LTLE4QGRVQ.jpg'},
+  {id:8, caption: '8th image', img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/8V46UZCS0V.jpg'}
+];
 
   constructor() { }
 
